70cf799b2f4b29b54ddbaa6244570b46364f9094
ahmad <aalmadani111@gmail.com>
2023-11-19 21:08:25 +0100

    add UI using bootstrap & font awsesome

--- src/components/ToDoApp.vue
+++ src/components/ToDoApp.vue
@@ -1,5 +1,42 @@
 <template>
-    <h2>Hello World</h2>
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-lg-6 mx-auto">
+                <h3 class="my-4 text-center">ToDo App</h3>
+                <div class="d-flex">
+                    <input type="text" class="form-control w-100 " placeholder="Enter your Task">
+                    <button class="btn btn-warning rounded ms-3">Add New Task</button>
+            </div>
+                    </div>
+                    <table class="table mt-5">
+                    <thead>
+                        <tr>
+
+                        <th scope="col">Task</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Edit</th>
+                        <th scope="col">Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <td>Study vuejs</td>
+                        <td>Witing</td>
+                        <td><div>
+                            <a href="#"><span class="fa fa-edit"></span></a>
+                            
+                        </div></td>
+                        <td><div>
+                            
+                            <a href="#"><span class="fa fa-times"></span></a>
+                        </div></td>
+                        </tr>
+
+                    </tbody>
+                    </table>
+
+                            </div>
+                        </div>
 </template>
 
 
@@ -7,5 +44,7 @@
 
 export default {
     name: "HelloWorld",
+
+
 }
 </script>
